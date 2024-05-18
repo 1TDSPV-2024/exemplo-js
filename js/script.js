643648ba9ed18fd9d4009700bbd46e531665d1f7
siqgabriel <gabriel.agos@live.com>
2024-05-17 21:28:08 -0300

    Ajuste

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@
 // console.log(nr.toFixed(2));
 
 
-// const btnElemento = document.getElementById("btnSubimit");
+// const btnElemento = document.getElementById("botao");
 
 // btnElemento.addEventListener("click",function (){
     
@@ -61,24 +61,128 @@
     
 // });
 
+//Recuperação de elementos no DOM
 
-// HTML Collection
-let listaLi = document.getElementsByTagName("li");
-console.log(listaLi);
+//Recupere o botão de submit do FORM, atrele o evento de mouseover e dispare um alert com a seguinte mensagem: "CONSEGUI";
 
-// Array
-const nr1 = [1, 2, 3, 4 ,5]
-console.log(nr1)
+// const botaoEnviar = document.getElementById("btnSubmit");
+// botaoEnviar.addEventListener("mouseover",function (){
+//     alert("CONSEGUI!");
+// });
 
-// Convertendo HTML Collection em Array com operador SPREAD (...);
-let novaListaLi = [...listaLi];
-console.log(novaListaLi);
-no
-const nr2 = [6,7,8,9,10];
-const novoArray = [...nr1,...nr2]
+// //HTMLCollection
+// let listaLi = document.getElementsByTagName("li");
+// console.log(listaLi);
 
-// removeItem(novoArray,8);
-// novoArray.unshift(8);
+// //Array
+// const nr1 = [1,2,3,4,5];
+// console.log(nr1);
+
+// //Convertendo HTMLCollection em Array com operador SPREAD ( ... );
+// let novaListaLi = [...listaLi];
+// console.log(novaListaLi);
+
+// const nr2 = [6,7,8,9,10];
+
+// const novoArray = [...nr1,...nr2];
 // console.log(novoArray);
 
+// //Removendo um item do array
+// // const removeItem = (array,item) => {
+// //     const indice = array.indexOf(item);
+// //     if(indice != -1 ) 
+// //         array.splice(indice,1);
+// //     else
+// //         console.log(`O valor não foi encontrado!`);
+// // }
 
+// // removeItem(novoArray,8);
+// // novoArray.unshift(8);
+// console.log(novoArray);
+
+// console.log(nr1.concat(nr2));
+
+
+
+
+//Recuperar os inputs Email e Senha e imprimir o atributo placeholder de ambos no console.
+
+// // const inputEmail = document.querySelector("input[placeholder='Digite seu email']");
+// const inputEmail = document.querySelector(".email");
+// console.log(inputEmail.placeholder);
+
+// const btnSubmit = document.querySelector("button[type=submit]");
+// // btnSubmit.innerText = "<p>TEXTO</p>";
+// btnSubmit.innerHTML = "<p>TEXTO</p>";
+// // console.log(btnSubmit.innerHTML);
+
+// btnSubmit.addEventListener("click", ()=>{
+//     console.log(inputEmail.value);
+// });
+
+//Lista de usuários com objetos:
+let listaUsuarios = [
+    {nome:"Joaquim José", email:"jo@email", senha:"123"},
+    {nome:"Maria José", email:"ma@email", senha:"123"},
+    {nome:"Antonio José", email:"an@email", senha:"123"},
+    {nome:"Luis José", email:"lu@email", senha:"123"},
+    {nome:"Gertudres José", email:"ge@email", senha:"123"},
+];
+
+function validaLogin(input1,input2,event) {
+
+    // event.preventDefault();
+
+    let msgStatus;
+    try{
+        msgStatus = document.querySelector(".valida");
+    
+
+for (let x = 0; x < listaUsuarios.length; x++) {    
+   if((input1.value == listaUsuarios[x].email) && (input2.value == listaUsuarios[x].senha) ){
+        msgStatus.setAttribute("class","sucesso");
+        msgStatus.innerText = "Login realizado com SUCESSO!";
+        
+        setTimeout(()=>{
+            msgStatus.setAttribute("class","valida");
+            msgStatus.innerText = "";
+            window.location.href = "../status/sucesso.html";
+        },3000);
+        
+        return true;
+    }
+}
+    msgStatus.setAttribute("class","erro");
+    msgStatus.innerText = "Nome de Usuário e ou senha inválidos!";
+
+    setTimeout(()=>{
+        msgStatus.setAttribute("class","valida");
+        msgStatus.innerText = "";
+        window.location.href = "../status/erro.html";
+    },3000);
+    
+}catch(error){
+    msgStatus = ( document.querySelector(".sucesso") ? document.querySelector(".sucesso") : document.querySelector(".erro"));
+}
+
+    return false;
+}
+
+// if((input1.value == listaUsuarios[0].email) && (input2.value == listaUsuarios[0].senha) ){
+//     console.log("Usuário validado com sucesso!");
+//     return true;
+// }else{
+//     console.log("Usuário não validado!");
+//     return false;
+// }
+
+// listaUsuarios.forEach( (item,indice,array)=>{
+
+//     if((input1.value == array[indice].email) && (input2.value == array[indice].senha) ){
+//         console.log(`Usuário ${item.nome} foi validado com sucesso!`);
+//         return true;
+//     }else{
+//         console.log("Usuário não validado!");
+//         return false;
+//     }
+// } );
